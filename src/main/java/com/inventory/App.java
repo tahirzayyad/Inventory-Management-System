1b200e3d09fec1a6a4b4b59c9d7de5693cf99333
Zay <zayyadtahir3@gmail.com>
2025-11-11 03:42:36 +0100

    Update App.java
    Added TODO comment for future inventory summary feature

---
+++ src/main/java/com/inventory/App.java
@@ -0,0 +1,10 @@
+package com.inventory;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Inventory Management System!");
+    }
+}
+
+
+// TODO: Add inventory summary feature later
